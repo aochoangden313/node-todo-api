20d0abbd53d97de7ad1cb49c965f9dbd28c4d839
unknown <quanlv.56@gmail.com>
2018-09-25 21:43:22 +0700

    example using mongo db to insert some data

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,10 +1,37 @@
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+// Version mongodb >= 3.0 need to change to this syntax not '(err, db) ==> (err, client)'
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     if (err) {
         return console.log('Unable to connect to MongoDB server');
     }
     console.log('COnnect to MongoDB Server');
 
-    db.close();
+    const db = client.db('TodoApp');
+
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false,
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert todo', err);
+        }
+        console.log('Insert to Todos sucessfully');
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    })
+
+    // Insert new doc into Usres (name, age, location)
+    db.collection('Users').insertOne({
+        name: 'Quan Le Van',
+        age: 26,
+        location: 'Ha Noi',
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert todo', err);
+        }
+        console.log('Insert to Users sucessfully');
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    client.close();
 });
