55e914a0d90dc82ee8834030a9cea4d706d6e1d4
unknown <quanlv.56@gmail.com>
2018-10-07 09:55:22 +0700

    generate auth token

--- server/models/user.js
+++ server/models/user.js
@@ -1,43 +1,59 @@
 var validator = require('validator');
 var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-// {
-//     email: 'quanlv.56@gmail.com',
-//     password: 'ksdjfhakjdfsdf',
-//     tokens: [{
-//         access: 'auth',
-//         token: 'dsafsdafasdfasdfafasdfdasf'
-//     }]
-// }
-
-var User = mongoose.model('Users', {
-    email: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true,
-        unique: true,
-        validate: {
-            validator: validator.isEmail,
-            message: '{VALUE} is not a valid email'
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-    },
-    tokens: [{
-        access: {
+var UserSchema = new mongoose.Schema(
+    {
+        email: {
             type: String,
             required: true,
+            minlength: 1,
+            trim: true,
+            unique: true,
+            validate: {
+                validator: validator.isEmail,
+                message: '{VALUE} is not a valid email'
+            }
         },
-        token: {
+        password: {
             type: String,
             required: true,
-        }
-    }]
+            minlength: 6,
+        },
+        tokens: [{
+            access: {
+                type: String,
+                required: true,
+            },
+            token: {
+                type: String,
+                required: true,
+            }
+        }]
+    }
+);
 
-});
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+  
+    return _.pick(userObject, ['_id', 'email']);
+}
+
+// We do not user arrow function here because it do not bind this value
+UserSchema.methods.generateAuthToken = function() {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports =  {User};
