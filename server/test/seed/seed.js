efda8dd4d2ab36e2afec6bf01e694d4c6ecfefe5
aochoangden313 <quanlv.56@gmail.com>
2018-10-07 21:54:52 +0700

    Security and authentication (#15)
    * add authentication
    
    * demo hashing and using webtoken
    
    * add content for hasing file
    
    * generate auth token
    
    * hashing password
    
    * testing POST /users and GET /users/me

---
+++ server/test/seed/seed.js
@@ -0,0 +1,58 @@
+const { ObjectID } = require('mongodb').ObjectID;
+const jwt = require('jsonwebtoken');
+
+const { Todo } = require('./../../models/todo');
+const { User } = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [{
+    _id: userOneId,
+    email: 'abc@123.com',
+    password: 'userOnePass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({ _id: userOneId, access: 'auth' }, 'abc123').toString(),
+    }]
+},
+{
+    _id: userTwoId,
+    email: 'abc2@123.com',
+    password: 'userTwoPass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({ _id: userOneId, access: 'auth' }, 'abc123').toString(),
+    }]
+}
+];
+
+const todos = [
+    {
+        _id: new ObjectID(),
+        text: 'First todo test item',
+        completed: true,
+        complatedAt: 333,
+    },
+    {
+        _id: new ObjectID(),
+        text: 'Second todo test item',
+    }
+];
+
+const populateTodos = (done) => {
+    Todo.remove({}).then(() => {
+        Todo.insertMany(todos);
+    }).then(() => done());
+};
+
+const populateUsers = (done) => {
+    User.remove({}).then(() => {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
+
+        return Promise.all([userOne, userTwo]);
+    }).then(() => done());
+};
+
+module.exports = { todos, populateTodos, users, populateUsers };
