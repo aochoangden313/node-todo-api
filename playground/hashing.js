99b1b6c02be2eb0e1184c652ee94f773ae1d0200
aochoangden313 <quanlv.56@gmail.com>
2018-10-07 16:17:18 +0700

    Security and authentication (#14)
    * add authentication
    
    * demo hashing and using webtoken
    
    * add content for hasing file
    
    * generate auth token
    
    * hashing password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(`Hashed pass: ${hash}`);
+//     });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$.JGmVPTA8eH.uwNCNOV3S.PK8QOlnRJjyYqJJZjy12eGsWbEr.zkO';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log('result: ' + res);
+})
+
+// var data = {
+//   id: 10
+// };
+
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
