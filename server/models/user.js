a24f94aa9e55f8b038e1c130e08cca31e90d5a26
unknown <quanlv.56@gmail.com>
2018-10-07 08:01:26 +0700

    add authentication

--- server/models/user.js
+++ server/models/user.js
@@ -1,20 +1,43 @@
+var validator = require('validator');
 var mongoose = require('mongoose');
 
+// {
+//     email: 'quanlv.56@gmail.com',
+//     password: 'ksdjfhakjdfsdf',
+//     tokens: [{
+//         access: 'auth',
+//         token: 'dsafsdafasdfasdfafasdfdasf'
+//     }]
+// }
+
 var User = mongoose.model('Users', {
-    user: {
-        type: String,
-        required: true,
-    },
     email: {
         type: String,
         required: true,
         minlength: 1,
         trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
     },
-    age: {
-        type: Number,
-        default: 0,
-    }
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true,
+        },
+        token: {
+            type: String,
+            required: true,
+        }
+    }]
+
 });
 
 module.exports =  {User};
